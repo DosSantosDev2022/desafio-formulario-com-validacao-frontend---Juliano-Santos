a2e4a3d4c4964da5f50e64d473386990af20e64d
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2024-07-30 00:18:04 -0300

    starting project settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
-
-import './App.css'
-
 function App() {
-
   return (
     <>
-      <main>
-        <h1>Seja bem vindo! :)</h1>
-        <p>Terceiro Desafio: Formulário de Cadastro de Membros</p>
-      </main>
+      <main></main>
     </>
-  );
+  )
 }
 
 export default App
