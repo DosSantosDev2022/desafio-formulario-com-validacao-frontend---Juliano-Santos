0559bcec7ee6a8bf4cda03950ec10db0be89985d
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2024-07-30 21:54:50 -0300

    FEAT: adding steps component

---
+++ src/components/steps.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+interface Step {
+  index: string
+  content: string
+}
+
+interface StepsProps {
+  steps: Step[]
+  currentStep: number
+  setCurrentStep: React.Dispatch<React.SetStateAction<number>>
+}
+
+export function Steps({ steps, currentStep, setCurrentStep }: StepsProps) {
+  return (
+    <div className="w-full  flex items-center justify-center  gap-1  ">
+      {steps.map((step, index) => (
+        <div
+          key={step.index}
+          className={`flex items-center justify-center gap-2  px-1.5 py-2 w-full cursor-pointer 
+          ${currentStep === index ? 'text-violet-900 font-bold' : 'text-zinc-500'}`}
+          onClick={() => setCurrentStep(index)}
+        >
+          <div
+            className={`bg-violet-900 rounded-full p-5 w-7 h-7 font-medium flex items-center justify-center
+               text-zinc-50
+               ${currentStep === index ? 'bg-violet-900' : 'bg-zinc-500'}`}
+          >
+            {step.index}
+          </div>
+          <button className="font-bold text-lg">{step.content} </button>
+        </div>
+      ))}
+    </div>
+  )
+}
